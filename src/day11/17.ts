28c0f2f4256fa8b472f180f31112b780b4671cc2
hinson0 <hinson0@qq.com>
2026-09-06 18:55:54 +0800

    完善Event Loop注释，增加timers、poll和check阶段的详细说明

--- src/day11/17.ts
+++ src/day11/17.ts
@@ -2,5 +2,30 @@
 
 /**
  * js -> 同步代码 -> process.nextTick -> promise / queueMicrotask -> event loop(timer -> poll -> check)
- * ->callback执行(nextTick -> microTask) -> 继续event loop
+ * -> 接下图
  */
+
+// Event Loop
+//    │
+//    ├─ timers
+//    │    └─ 执行一个 timer callback
+//    │           ↓
+//    │       nextTick
+//    │           ↓
+//    │       microtask
+//    │
+//    ├─ poll
+//    │    └─ 执行一个 I/O callback
+//    │           ↓
+//    │       nextTick
+//    │           ↓
+//    │       microtask
+//    │
+//    └─ check
+//         └─ 执行一个 setImmediate callback
+//                ↓
+//            nextTick
+//                ↓
+//            microtask
+
+// 不是`某个阶段清microtask`,而是一个callback执行完,就清nextTick/microtask
